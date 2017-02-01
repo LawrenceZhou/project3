efe5288139619e8f70f44c20992af7e84cfa1c05
Yijun Zhou <hmskyijun@gmail.com>
2017-01-31 16:10:53 -0800

    Add files via upload

---
+++ DatePicker.js
@@ -0,0 +1,44 @@
+class DatePicker {
+  constructor(id, callback) {
+    this.id = id;
+
+    if(one box get clikced) {
+    	callback(this.id, one box);
+    }
+  }
+
+  render(date) {
+  	var calendar = document.getElementById(this.id);
+  	var monthRow = document.createElement("DIV");
+  	monthRow.setAttribute("id", "month");
+  	var list = document.createElement("UL");
+
+  	var prev = document.createElement("LI");
+  	prev.setAttribute("class", "prev");
+  	prev.innerHTML = "<";
+
+  	var next = document.createElement("LI");
+  	next.setAttribute("class", "next");
+  	prev.innerHTML = ">";
+
+  	var title = document.createElement("LI");
+  	title.setAttribute("style", "text-align:center");
+  	title.innerHTML = "August<br><span style=\"font-size:18px\">2016</span>";
+
+  	list.appendChild(prev);
+  	list.appendChild(next);
+  	list.appendChild(title);
+  	monthRow.appendChild(list);
+  	calendar.appendChild(monthRow);
+
+//  <ul>
+//    <li class="prev">❮</li>
+//    <li class="next">❯</li>
+//    <li style="text-align:center">
+//      August<br>
+//      <span style="font-size:18px">2016</span>
+//    </li>
+//  </ul>
+//</div>
+  }
+}
