685dd7184346e89f74a139c67eeb84bf893e1905
LawrenceZhou <hmskyijun@gmail.com>
2017-02-01 11:48:36 -0800

    37+

--- TableTemplate.js
+++ TableTemplate.js
@@ -8,7 +8,7 @@ class TableTemplate {
 		var tds = table.getElementsByTagName("td");
 
 		if(arguments.length === 2) {
-			for (i = 0; i < tds.length; i++) {
+			for (var i = 0; i < tds.length; i++) {
 				tds[i].innerHTML = Cs142TemplateProcessor(tds[i].innerHTML).fiiIn(dictionary);
 			}
 		}else if(arguments.length === 3) {
