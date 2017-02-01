d535dc3dd915cefef81cdfa7ebfb770db072fd1a
LawrenceZhou <hmskyijun@gmail.com>
2017-02-01 11:39:51 -0800

    37

--- TableTemplate.js
+++ TableTemplate.js
@@ -1,5 +1,17 @@
 function TableTemplate () {
 	static fillIn(id, dictionary, columnName) {
+		var id = id;
+		var dictionary = dictionary;
+		var table = document.getElementById(id);
+		var tds = table.getElementsByTagName("td");
+
+		if(arguments.length === 2) {
+			for (i = 0; i < tds.length; i++) {
+				Cs142TemplateProcessor(tds[i].innerHTML).fiiIn(dictionary);
+			}
+		}else if(arguments.length === 3) {
+
+		}
 
 	}
 }
