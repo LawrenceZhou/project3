22adb4b19ace8e7aed1bdc330a98c54aef444f14
LawrenceZhou <hmskyijun@gmail.com>
2017-01-31 16:29:45 -0800

    3rd

--- DatePicker.js
+++ DatePicker.js
@@ -1,12 +1,24 @@
 function DatePicker(id, callback) {
     this.id = id;
+    this.monthList =new Array(12);
+    monthList[0]="January";
+    monthList[1]="February";
+    monthList[2]="March";
+    monthList[3]="April";
+    monthList[4]="May";
+    monthList[5]="June";
+    monthList[6]="July";
+    monthList[7]="August";
+    monthList[8]="September";
+    monthList[9]="October";
+    monthList[10]="November";
+    monthList[11]="December";
 
     //if(one box get clikced) {
    // 	callback(this.id, one box);
     //}
   //}
 
-  
 
   this.render = function(date) {
   	var calendar = document.getElementById(this.id);
@@ -24,7 +36,8 @@ function DatePicker(id, callback) {
 
   	var title = document.createElement("LI");
   	title.setAttribute("style", "text-align:center");
-  	title.innerHTML = "August<br><span style=\"font-size:18px\">2016</span>";
+    title.innerHTML = this.monthList[date.getMonth()];
+  	title.innerHTML += "<br><span style=\"font-size:18px\">2016</span>";
 
   	list.appendChild(prev);
   	list.appendChild(next);
