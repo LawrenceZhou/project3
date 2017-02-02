bf746a27532ad798ee46a9418b9d25a63c98931d
LawrenceZhou <hmskyijun@gmail.com>
2017-02-01 19:55:54 -0800

    39

--- TableTemplate.js
+++ TableTemplate.js
@@ -10,13 +10,29 @@ class TableTemplate {
 		if(arguments.length === 2) {
 			var tds = table.getElementsByTagName("td");
 			for (var i = 0; i < tds.length; i++) {
-				console.log(tds[i].innerHTML);
 				var template = new Cs142TemplateProcessor(tds[i].innerHTML);
 				tds[i].innerHTML = template.fillIn(dictionary);
-				console.log(tds[i].innerHTML);
 			}
 		}else if(arguments.length === 3) {
-			var 
+			var headersrow = table.getElementsByClassName("header");
+			var headers = headersrow[0].getElementsByTagName("td");
+			var columnNumber = headers.length;
+			var columntoChange = 0;
+			for (var i = 0; i < headers.length; i++) {
+				if(dictionary[headers[i].innerHTML.slice(2, -2)] === columnName) {
+					columntoChange = i;
+					break;
+				}
+			}
+
+			var rows = table.getElementsByTagName("tr");
+			//console.log(rows.length);
+			for (var i = 0; i < rows.length; i++) {
+				var tdtoChange = rows[i].getElementsByTagName("td")[columntoChange];
+				//console.log(tdtoChange);				
+				var template = new Cs142TemplateProcessor(tdtoChange.innerHTML);
+				tdtoChange.innerHTML = template.fillIn(dictionary);
+			}
 		}
 
 	}
