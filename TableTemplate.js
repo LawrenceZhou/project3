16ba4d1e5733f866c2bced6a1d20fb9383ea47c3
LawrenceZhou <hmskyijun@gmail.com>
2017-02-01 18:32:21 -0800

    38

--- TableTemplate.js
+++ TableTemplate.js
@@ -6,22 +6,15 @@ class TableTemplate {
 		var id = id;
 		var dictionary = dictionary;
 		var table = document.getElementById(id);
-		var tds = table.getElementsByTagName("td");
-
+		
 		if(arguments.length === 2) {
+			var tds = table.getElementsByTagName("td");
 			for (var i = 0; i < tds.length; i++) {
 				console.log(tds[i].innerHTML);
 				var template = new Cs142TemplateProcessor(tds[i].innerHTML);
 				tds[i].innerHTML = template.fillIn(dictionary);
 				console.log(tds[i].innerHTML);
-				//tds[i].innerHTML = Cs142TemplateProcessor(table.innerHTML).fiilIn(dictionary);
 			}
-			//console.log(table.innerHTML);
-			//Cs142TemplateProcessor(table.innerHTML);
-			//var template = new Cs142TemplateProcessor(table.innerHTML);
-			//console.log(template, typeof(template.template));
-			//table.innerHTML = template.fillIn(dictionary);
-			//console.log(table.innerHTML);
 		}else if(arguments.length === 3) {
 
 		}
